3aa8066bea4eacc2207d76f0059c1c21f8315262
임승진 <seungjin051@gmail.com>
2025-11-17 14:08:11 +0900

    feat: 개발 환경에서 React Query Devtools 추가

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import './styles/global.css'
 import App from './App'
 import { queryClient } from '@/api/queryClient'
@@ -12,6 +13,7 @@ createRoot(document.getElementById('root')!).render(
       <BrowserRouter>
         <App />
       </BrowserRouter>
+      {import.meta.env.DEV && <ReactQueryDevtools initialIsOpen={false} />}
     </QueryClientProvider>
   </StrictMode>,
 )
